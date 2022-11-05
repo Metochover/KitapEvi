97031113926db0052ce5e3331a29bfa3e3fa30bb
Metochover <33metehan23@gmail.com>
2022-11-06 00:48:02 +0300

    KitapEvi

---
+++ GRSYKitapEvi/Model/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace GRSYKitapEvi.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
